beb7d907c00473aa4629167627c1e01f9bd4171a
Monika Jakubowska <mjakubowska.official@gmail.com>
2024-05-11 12:27:01 +0200

    Edited ChoosingAndDecliningCookies test

--- tests/TS05_ChoosingAndDecliningCookies.spec.ts
+++ tests/TS05_ChoosingAndDecliningCookies.spec.ts
@@ -8,22 +8,32 @@ test.describe('Choosing cookies', () => {
     generalPage = new GeneralPage(page);
 
     await page.goto('/');
+    await generalPage.clickCustomizeCookiesOnPage();
   });
 
   test('Should be able to choose performance and functionality cookies', async ({
     page,
   }) => {
-    await generalPage.clickCustomizeCookiesOnPage();
-    await expect(page.getByText('This website uses cookies')).toBeVisible();
+    // Arrange
+    const expectedCookieTitle = 'This website uses cookies';
+    const cookieModal = '#cookiescript_injected';
+
+    // Act
+    await expect(page.getByText(expectedCookieTitle)).toBeVisible();
     await generalPage.choosePerformanceAndFunctionalityCookiesOnPage();
     await generalPage.saveSettingsOfCookiesAndClose();
-    await expect(page.locator('#cookiescript_injected')).toBeHidden();
+
+    // Assert
+    await expect(page.locator(cookieModal)).toBeHidden();
   });
 
   test('Should be able to decline all cookies', async ({ page }) => {
-    await generalPage.clickCustomizeCookiesOnPage();
-    await expect(page.getByText('This website uses cookies')).toBeVisible();
+    // Arrange
+    const cookieModal = '#cookiescript_injected';
+    // Act
     await generalPage.clickDeclineCookies();
-    await expect(page.locator('#cookiescript_injected')).toBeHidden();
+
+    // Assert
+    await expect(page.locator(cookieModal)).toBeHidden();
   });
 });
