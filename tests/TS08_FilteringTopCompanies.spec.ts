4b75ff8b010d991329bd7df9b5b0165467d3f060
Monika Jakubowska <mjakubowska.official@gmail.com>
2024-05-14 19:36:41 +0200

    Edited TS08_FilteringTopCompanies test

--- tests/TS08_FilteringTopCompanies.spec.ts
+++ tests/TS08_FilteringTopCompanies.spec.ts
@@ -1,39 +1,40 @@
 import { FiltersPage } from '../src/pages/filters.page';
 import { GeneralPage } from '../src/pages/general.page';
-import { HeaderPage } from '../src/pages/header.page';
 import { TopCompaniesPage } from '../src/pages/topCompanies.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Filtering top companies', () => {
   let generalPage: GeneralPage;
-  let headerPage: HeaderPage;
   let topCompaniesPage: TopCompaniesPage;
   let filtersPage: FiltersPage;
 
   test.beforeEach(async ({ page }) => {
     generalPage = new GeneralPage(page);
-    headerPage = new HeaderPage(page);
     topCompaniesPage = new TopCompaniesPage(page);
     filtersPage = new FiltersPage(page);
 
     await page.goto('/brands');
     await generalPage.clickAcceptCookiesOnPage();
-    await expect(page.locator('#cookiescript_injected')).toBeHidden();
-    await page.waitForLoadState();
-    await page.waitForURL('/brands', { waitUntil: 'domcontentloaded' });
   });
 
   test('Should be able to filtering top companies by type of company', async ({
     page,
   }) => {
-    await headerPage.clickTopCompaniesButtonOnTheMainPage();
-    await page.waitForURL('/brands', { waitUntil: 'domcontentloaded' });
+    // Arrange
+    const expectedChosenTypeOfCompany_1 = 'Software House';
+    const expectedChosenTypeOfCompany_2 = 'Corporation';
+
+    // Act
     await topCompaniesPage.clickSoftwareHouseButton();
-    await page.waitForLoadState();
-    await expect(page.locator('ul li').first()).toContainText('Software House');
     await topCompaniesPage.clickCorporationButton();
-    await page.waitForLoadState();
-    await expect(page.locator('ul li').nth(1)).toContainText('Corporation');
+
+    // Assert
+    await expect(page.locator('ul li').first()).toContainText(
+      expectedChosenTypeOfCompany_1,
+    );
+    await expect(page.locator('ul li').nth(1)).toContainText(
+      expectedChosenTypeOfCompany_2,
+    );
   });
 
   test('Should be able to filtering top companies by name', async ({
