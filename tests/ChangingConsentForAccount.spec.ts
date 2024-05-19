8d8a74b2a4a48655312554611601fb2fae806439
Monika Jakubowska <mjakubowska.official@gmail.com>
2024-05-19 23:32:57 +0200

    Edited ChangingConsentForAccount test

--- tests/ChangingConsentForAccount.spec.ts
+++ tests/ChangingConsentForAccount.spec.ts
@@ -1,41 +1,35 @@
-/* eslint-disable playwright/expect-expect */
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* eslint-disable playwright/no-focused-test */
 import { CandidateAccountPage } from '../src/pages/candidateAccount.page';
-import { FormsPage } from '../src/pages/forms.page';
 import { GeneralPage } from '../src/pages/general.page';
 import { HeaderPage } from '../src/pages/header.page';
 import { LoginPage } from '../src/pages/login.page';
+import { candidate1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Changing consent for the account', () => {
   let loginPage: LoginPage;
   let generalPage: GeneralPage;
   let headerPage: HeaderPage;
-  let formsPage: FormsPage;
   let candidateAccountPage: CandidateAccountPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     generalPage = new GeneralPage(page);
     headerPage = new HeaderPage(page);
-    formsPage = new FormsPage(page);
     candidateAccountPage = new CandidateAccountPage(page);
 
     await page.goto('/');
     await generalPage.clickAcceptCookiesOnPage();
-    await expect(page.locator('#cookiescript_injected')).toBeHidden();
   });
 
   test('Should be able to change consent for the account', async ({ page }) => {
+    // Act
     await headerPage.goToSignInPageForCandidateFromPageHeader();
     await loginPage.goToSignInPageByEmail();
-    await formsPage.fillFormToLoginAsCandidate();
-    await loginPage.clickSignInButton();
+    await loginPage.loginCandidateAccount(candidate1);
     await candidateAccountPage.goToSettingsFromSideMenu();
     await candidateAccountPage.changeConsent();
+
+    // Assert
     await expect(page.getByRole('checkbox')).not.toBeChecked();
   });
 });
